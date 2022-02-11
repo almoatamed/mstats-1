b9628ae7c9b84cd1bdf6f08355bdf2fa5d3917d9
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-11 04:56:56 +0200

    user store module authentication:   - token verifcation   - token clear/set   - login   - logout   - verify
    notification, store retriev, loading_cover, abstract visuals

--- client/admin/src/store/modules/user.js
+++ client/admin/src/store/modules/user.js
@@ -1,67 +1,159 @@
+/* eslint-disable */
 // Utilities
 import { make } from 'vuex-pathify'
+
+// user api
+import userApi from '../../api/user/index'
 
 // Globals
 import { IN_BROWSER } from '@/util/globals'
 
-const state = {
-  dark: false,
-  drawer: {
-    image: 0,
-    gradient: 0,
-    mini: false,
+// router 
+import router from '../../router/index'
+
+const state = JSON.parse(localStorage.getItem('aramtech@user')) || {
+  visualization: {
+    loading_cover: false,
+    dark: false,
+    drawer: {
+      image: 0,
+      gradient: 0,
+      mini: false,
+    },
+    gradients: [
+      'rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)',
+      'rgba(228, 226, 226, 1), rgba(255, 255, 255, 0.7)',
+      'rgba(244, 67, 54, .8), rgba(244, 67, 54, .8)',
+    ],
+    images: [
+    ],
+    rtl: false,
+
   },
-  gradients: [
-    'rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)',
-    'rgba(228, 226, 226, 1), rgba(255, 255, 255, 0.7)',
-    'rgba(244, 67, 54, .8), rgba(244, 67, 54, .8)',
-  ],
-  images: [
-    'https://demos.creative-tim.com/material-dashboard-pro/assets/img/sidebar-1.jpg',
-    'https://demos.creative-tim.com/material-dashboard-pro/assets/img/sidebar-2.jpg',
-    'https://demos.creative-tim.com/material-dashboard-pro/assets/img/sidebar-3.jpg',
-    'https://demos.creative-tim.com/material-dashboard-pro/assets/img/sidebar-4.jpg',
-  ],
-  notifications: [],
-  rtl: false,
+  notification: {
+    notification: false,
+    msg:'',
+    color:'warning',
+    v:'top',
+    h:'center',
+  },
+  auth:{
+    token: ''
+  }
 }
 
-const mutations = make.mutations(state)
+const mutations = {
+  // default mudations
+  ...make.mutations(state),
+  DARK(state,value){
+    state.visualization.dark = value
+  },
+  TOKEN(state, value){
+    state.auth.token = value
+  },
+  NOTIFY(state, notification){
+    state.notification.notification =true
+    state.notification.msg = notification.msg 
+    state.notification.color = notification.color
+    state.notification.h = 'center'
+    if(router.currentRoute.name.indexOf('dashboard') == -1){
+      state.notification.v = 'top'
+    }else{
+      state.notification.v = 'bottom'
+    }
+  }
+}
 
 const actions = {
-  fetch: ({ commit }) => {
-    const local = localStorage.getItem('vuetify@user') || '{}'
-    const user = JSON.parse(local)
-
-    for (const key in user) {
-      commit(key, user[key])
+  
+  // auth   
+  set_token(context,token){
+    console.log('commiting token ', token)
+    if(token){
+      context.commit('TOKEN', token)
+    }else{
+      context.commit('TOKEN', '')
     }
+    context.dispatch('update_localstorage')
+  },
+  verify(){
+    return new Promise((resolve, reject)=>{
+      userApi.verify().then(res=>{resolve(res)}).catch(err=>{reject(err)})
+    })
+  },
 
-    if (user.dark === undefined) {
-      commit('dark', window.matchMedia('(prefers-color-scheme: dark)'))
+
+  // login/logout
+  login(context, body){
+    return new Promise((resolve, reject)=>{
+      userApi.login(body).then(res => {
+            context.dispatch('user/set_token', res.data.result.token,{root:true})
+            resolve(res)
+        }).catch(err => {
+            context.dispatch('user/set_token',null, {root:true})
+            reject(err);
+        })
+    })
+  },
+  logout(context){
+    context.dispatch('set_token')
+    if(router.currentRoute.fullPath != '/'){
+      router.push('/')
     }
   },
-  update: ({ state }) => {
-    if (!IN_BROWSER) return
 
-    localStorage.setItem('vuetify@user', JSON.stringify(state))
+
+  // notification 
+  notify(context,notification){
+    context.commit("NOTIFY", notification)
+  },
+
+
+  // local storage 
+  fetch_localstorage: async ({ commit }) => {
+    const local = await localStorage.getItem('aramtech@user') || '{}'
+    const user = await JSON.parse(local)
+
+    for (const key in user) {
+      await commit(key, user[key])
+    }
+
+    if (user.visualization.dark === undefined) {
+      await commit('DARK', window.matchMedia('(prefers-color-scheme: dark)'))
+    }
+    return new Promise(resolve=>resolve())
+  },
+  update_localstorage: ({ state }) => {
+    if (!IN_BROWSER) return
+    localStorage.setItem('aramtech@user', JSON.stringify(state))
   },
 }
 
 const getters = {
+  // auth 
+  auth: (state) =>{
+    return !!state.auth.token
+  },
+  token: (state) => {
+    return state.auth.token
+  },
+
+  // visual getters 
   dark: (state, getters) => {
     return (
-      state.dark ||
-      getters.gradient.indexOf('255, 255, 255') === -1
+      state.visualization.dark ||
+      getters.visualization.gradient.indexOf('255, 255, 255') === -1
     )
   },
   gradient: state => {
-    return state.gradients[state.drawer.gradient]
+    return state.visualization.gradients[state.visualization.drawer.gradient]
   },
   image: state => {
-    return state.drawer.image === '' ? state.drawer.image : state.images[state.drawer.image]
+    return state.visualization.drawer.image === '' ? state.visualization.drawer.image : state.visualization.images[state.visualization.drawer.image]
   },
 }
+
+
 
 export default {
   namespaced: true,

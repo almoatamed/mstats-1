617ad3118bdab16bee26d9a0c6b5b1e453999a8c
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-13 01:09:27 +0200

    included status codes

--- server/env.js
+++ server/env.js
@@ -1,4 +1,14 @@
 module.exports = {
+    response:{
+        status_codes:{
+            invalid_field:401,
+            not_autharized:421,
+            not_found:404,
+            server_error:500,
+            repeated_query:406,
+            ok:200,
+        }
+    },
     db: {
         mysql: {
             host: "localhost",

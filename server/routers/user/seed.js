9e9682d203d919c282992a6d9434115f8856e210
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-13 01:11:05 +0200

    modified seed to abstract status codes

--- server/routers/user/seed.js
+++ server/routers/user/seed.js
@@ -6,11 +6,11 @@ const bcrypt = require('bcrypt')
  
 router.get('/',(req,res)=>{
     if(req.query.key != env.auth.seed_key){
-        return res.status(421).json({error:{name:'not authorized', msg:"please provide seed key"}})
+        return res.status(env.response.status_codes.not_autharized).json({error:{name:'not authorized', msg:"please provide seed key"}})
     }
     mysql.pool.getConnection((err,connection)=>{
         if(err){
-            res.status(500).json({error:{err, msg:'Error in database connection'}}).end()
+            res.status(env.response.status_codes.server_error).json({error:{err, msg:'Error in database connection'}}).end()
         }
         const sql_query = `
             SELECT * FROM user WHERE user_name = 'admin' and name = 'admin' AND deleted = 0 limit 1;
@@ -18,12 +18,12 @@ router.get('/',(req,res)=>{
         connection.query(sql_query,async (err, result) => {
             if(err){
                 connection.release()
-                res.status(500).json({error:{err, msg:'Error in database connection'}}).end()
+                res.status(env.response.status_codes.server_error).json({error:{err, msg:'Error in database connection'}}).end()
             }
             
-            const hash = await bcrypt.hash('admin',env.auth.bcrypt.rounds)
             
             if(result.length == 0){
+                const hash = await bcrypt.hash('admin',env.auth.bcrypt.rounds)
                 const seed_query = `
                     INSERT INTO user(name, user_name, password) VALUES(
                         'admin', 
@@ -34,14 +34,14 @@ router.get('/',(req,res)=>{
                 connection.query(seed_query, (err, result)=>{
                     connection.release()
                     if(err){
-                        res.status(500).json({error:{err, msg:'Error in seeding'}}).end()
+                        res.status(env.response.status_codes.server_error).json({error:{err, msg:'Error in seeding'}}).end()
                     }else{
-                        res.status(200).json({result:{msg:'defaul user created'}}).end()
+                        res.status(env.response.status_codes.ok).json({result:{msg:'defaul user created'}}).end()
                     }
                 })
             }else{
                 connection.release()
-                res.status(400).json({
+                res.status(env.response.status_codes.repeated_query).json({
                     error:{name:"seed error", message: "already seeded"},
                 }).end()
             }

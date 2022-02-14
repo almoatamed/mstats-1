5d1151952a973bbf6e18df2586fd79f1242ae129
wajeeh-aram <wajeeh@aramtech.ly>
2022-02-14 02:33:41 +0200

    hospital_api

--- server/routers/main.js
+++ server/routers/main.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/user', require('./user/main'))
+router.use('/hospital', require('./Hospital/HospRegistration'))
 
 
 module.exports = router

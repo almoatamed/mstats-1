8cd7b38837c8566f386042c5c79b777b8cce8c8a
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-17 14:31:33 +0200

    random username generator

---
+++ server/utils/generator/username.generator.js
@@ -0,0 +1,9 @@
+
+module.exports = function(name) {
+    let initial = '_'
+    if(name){
+        initial = name.match(/\b(\w)/g).join('')
+    }
+    return initial+Math.floor(Math.random()*10)+Math.floor(Math.random()*10)+Math.floor(Math.random()*10)
+
+}

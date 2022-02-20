2caac3549135ddfc860dc020f3f15b6827a7d580
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-20 04:32:12 +0200

    User status table

---
+++ client/admin/src/views/User/UserStatusTable.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- eslint-disable -->
+  <filtered-data-table
+    :deleted="0"
+    :table_actions="table_actions"
+    :headers="headers"
+    api_url="/user/fetch"
+    title="Users"
+    subtitle="User Details Table"
+    :no_search="false"
+    :no_filter="false"
+    :filters="filters"
+    filter_icon="mdi-filter-variant"
+
+    @log="log($event)"
+  />
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data: () => ({
+    headers: [
+      { text: "Username", value: "user_name" },
+      { text: "Name", value: "name" },
+      { text: "Phone", value: "phone_number" },
+      { text: "Address", value: "address" },
+      { text: "Last Modified", value: "updated_at" }
+    ],
+    filters: {
+      datetime: {
+        component: {
+          tab: {
+            title: "Date And Time",
+            icon: "mdi-calendar-clock"
+          },
+          name: "date-time-filter"
+        },
+        values: {}
+      },
+      user: {
+        component: {
+          tab: {
+            title: "By User",
+            icon: "mdi-account"
+          },
+          name: "user-filter"
+        },
+        values: {}
+      }
+    },
+    table_actions: {
+      name: "Table Actions",
+      actions: [
+        {
+          event: "log",
+          icon: "mdi-heart"
+        },
+        {
+          event: "log",
+          icon: "mdi-heart"
+        },
+        {
+          event: "log",
+          icon: "mdi-heart"
+        }
+      ]
+    }
+  }),
+  methods:{
+    log(event){
+      console.log(event.item) // event.item is the object containing the row data
+      event.refresh() // refresh is the method used to refresh the table if needed
+    }
+  }
+};
+</script>
+
+<style></style>

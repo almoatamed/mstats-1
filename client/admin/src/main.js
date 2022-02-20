9de89eab8bee882c9edcecbeee074ed7daa36a6a
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-20 04:23:06 +0200

    included extenstion for date and time

--- client/admin/src/main.js
+++ client/admin/src/main.js
@@ -6,6 +6,10 @@ import './plugins'
 import store from './store'
 import { sync } from 'vuex-router-sync'
 
+import { Datetime } from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
+Vue.use(Datetime)
+
 Vue.config.productionTip = false
 
 sync(store, router)

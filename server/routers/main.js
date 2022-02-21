abae3f9b9e002588899fdb235c413add132d94a1
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-21 12:45:25 +0200

    included the main routes of hospital and medrep

--- server/routers/main.js
+++ server/routers/main.js
@@ -3,9 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/user', require('./user/main'))
-router.use('/hospital', require('./Hospital/HospRegistration'))
-router.use('/MedRep', require('./MedRep/MedRepRegistration'))
-
-
+router.use('/hospital', require('./Hospital/main'))
+router.use('/medrep', require('./MedRep/main')) // paths should be small letters not camel
 
 module.exports = router

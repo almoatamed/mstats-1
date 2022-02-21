7b93141befdbb4a4a6458c0a1de20b8d79385399
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-21 20:53:18 +0200

    include pharmacy and doctor apis

--- server/routers/main.js
+++ server/routers/main.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 router.use('/user', require('./user/main'))
 router.use('/hospital', require('./Hospital/main'))
+router.use('/doctor', require('./Doctor/main'))
+router.use('/pharmacy', require('./Pharmacy/main'))
 router.use('/medrep', require('./MedRep/main')) // paths should be small letters not camel
 
 module.exports = router

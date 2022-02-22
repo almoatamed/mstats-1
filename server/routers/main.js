a00a8e00d57e2e20b4b61dd4113321bd9c00db0c
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-22 21:58:18 +0200

    adding routes for product and manufacturer

--- server/routers/main.js
+++ server/routers/main.js
@@ -6,6 +6,8 @@ router.use('/user', require('./user/main'))
 router.use('/hospital', require('./Hospital/main'))
 router.use('/doctor', require('./Doctor/main'))
 router.use('/pharmacy', require('./Pharmacy/main'))
+router.use('/manufacturer', require('./Manufacturer/main'))
 router.use('/medrep', require('./MedRep/main')) // paths should be small letters not camel
+router.use('/product', require('./Products/main')) // paths should be small letters not camel
 
 module.exports = router

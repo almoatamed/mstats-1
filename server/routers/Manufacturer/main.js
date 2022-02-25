6c3d53576180c7b4f7fd4ba1583e3667b5498c7d
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-25 14:55:30 +0200

    added fetch

--- server/routers/Manufacturer/main.js
+++ server/routers/Manufacturer/main.js
@@ -3,4 +3,5 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/register',require('./registration'))
+router.use('/fetch',require('./fetch'))
 module.exports = router

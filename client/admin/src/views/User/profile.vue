168a45fa3eea168ce3011fd47df1f2108ad8369b
Salem Elmotamed <s.elmotamed96@gmail.com>
2022-02-25 14:49:05 +0200

    user profile page

---
+++ client/admin/src/views/User/profile.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container
+    id="user-profile-view"
+    fluid
+    tag="section"
+  >
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <app-card class="mt-4 text-center">
+          <v-img
+            class="rounded-circle elevation-6 mt-n12 d-inline-block"
+            src="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
+            width="128"
+          />
+
+          <v-card-text class="text-center">
+            <h6 class="text-h6 mb-2 text--secondary">
+              CEO / FOUNDER
+            </h6>
+
+            <h4 class="text-h4 mb-3 text--primary">
+              John Leider
+            </h4>
+
+            <p class="text--secondary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione dolorem deserunt veniam tempora magnam quisquam quam error iusto cupiditate ducimus, et eligendi saepe voluptatibus assumenda similique temporibus placeat animi dicta?
+            </p>
+
+            <v-btn
+              class="mr-0"
+              color="primary"
+              min-width="100"
+              rounded
+            >
+              Follow
+            </v-btn>
+          </v-card-text>
+        </app-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default { name: 'UserProfileView' }
+</script>
